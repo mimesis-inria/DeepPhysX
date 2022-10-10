3126dc6121535d6fadc5cef41ccf9b94d413ac5a
robin <robin.enjalbert@inria.fr>
2022-10-10 14:32:36 +0200

    Rendering per instance is only triggered when Factories are remotely managed.

--- src/Visualization/VedoVisualizer.py
+++ src/Visualization/VedoVisualizer.py
@@ -47,7 +47,6 @@ class VedoVisualizer(_VedoVisualizer):
         if not remote:
             self.__database.register_post_save_signal(table_name='Sync',
                                                       handler=self.__sync_visualizer)
-        self.remote = remote
 
     def render_instance(self, instance: int):
         """
@@ -56,32 +55,28 @@ class VedoVisualizer(_VedoVisualizer):
         :param instance: Index of the Environment render to update.
         """
 
-        if not self.remote:
-            self.render()
+        # 1. Update Factories steps
+        if instance not in self.step:
+            self.step[instance] = 0
+        self.step[instance] += 1
 
-        else:
-            # 1. Update Factories steps
-            if instance not in self.step:
-                self.step[instance] = 0
-            self.step[instance] += 1
+        # 2. Retrieve visual data and update Actors (one Table per Actor)
+        table_names = self.__database.get_tables()
+        table_names.remove('Sync')
+        table_names = [table_name for table_name in table_names if table_name.split('_')[1] == str(instance)]
+        for table_name in table_names:
+            # Get the current step line in the Table
+            data_dict = self.__database.get_line(table_name=table_name,
+                                                 line_id=self.step[instance])
+            # If the id of line is correct, the Actor was updated
+            if data_dict.pop('id') == self.step[instance]:
+                self.update_instance(table_name=table_name,
+                                     data_dict=data_dict)
+            # Otherwise, the actor was not updated, then add an empty line
+            else:
+                self.__database.add_data(table_name=table_name,
+                                         data={})
 
-            # 2. Retrieve visual data and update Actors (one Table per Actor)
-            table_names = self.__database.get_tables()
-            table_names.remove('Sync')
-            table_names = [table_name for table_name in table_names if table_name.split('_')[1] == str(instance)]
-            for table_name in table_names:
-                # Get the current step line in the Table
-                data_dict = self.__database.get_line(table_name=table_name,
-                                                     line_id=self.step[instance])
-                # If the id of line is correct, the Actor was updated
-                if data_dict.pop('id') == self.step[instance]:
-                    self.update_instance(table_name=table_name,
-                                         data_dict=data_dict)
-                # Otherwise, the actor was not updated, then add an empty line
-                else:
-                    self.__database.add_data(table_name=table_name,
-                                             data={})
-
-            # 3. Render Plotter if offscreen is False
-            if not self.__offscreen:
-                self.__plotter.render()
+        # 3. Render Plotter if offscreen is False
+        if not self.__offscreen:
+            self.__plotter.render()
